a9be8592c11e01093549e4e04f82e0c2493bfca2
thang <63875614+mingthang@users.noreply.github.com>
2025-11-28 22:54:09 +0700

    Add init main window implementation

--- DX_Common/Private/D3DApp.cpp
+++ DX_Common/Private/D3DApp.cpp
@@ -4,6 +4,14 @@
 #include "D3DUtil.h"
 #include "directx/d3dx12.h"
 
+LRESULT CALLBACK
+MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	// Forward hwnd on because we can get messages (e.g., WM_CREATE)
+	// before CreateWindow returns, and thus before mhMainWnd is valid.
+	return D3DApp::GetApp()->MsgProc(hwnd, msg, wParam, lParam);
+}
+
 D3DApp::~D3DApp()
 {
 	// The destructor releases the COM interfaces the D3DApp acquires, 
@@ -109,7 +117,41 @@ bool D3DApp::Initialize()
 
 bool D3DApp::InitMainWindow()
 {
+	WNDCLASS wc;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = MainWndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = m_hAppInst;
+	wc.hIcon = LoadIcon(0, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(0, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
+	wc.lpszMenuName = 0;
+	wc.lpszClassName = L"MainWnd";
 
+	if (!RegisterClass(&wc))
+	{
+		MessageBox(0, L"RegisterClass Failed.", 0, 0);
+		return false;
+	}
+
+	RECT R = { 0, 0, m_ClientWidth, m_ClientHeight };
+	AdjustWindowRect(&R, WS_OVERLAPPEDWINDOW, false);
+	int width = R.right - R.left;
+	int height = R.bottom - R.top;
+
+	m_hMainWnd = CreateWindow(L"MainWnd", m_MainWndCaption.c_str(),
+		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, width, height, 0, 0, m_hAppInst, 0);
+	if (!m_hMainWnd)
+	{
+		MessageBox(0, L"CreateWindow Failed.", 0, 0);
+		return false;
+	}
+
+	ShowWindow(m_hMainWnd, SW_SHOW);
+	UpdateWindow(m_hMainWnd);
+
+	return true;
 }
 
 bool D3DApp::InitDirect3D()
